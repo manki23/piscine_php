f0f6ff6e4d291a87b4791114c8a0dcdba51c76ad
Myriam ANKI <manki@student.42.fr>
2019-10-26 18:38:14 +0200

    day09/ jquery part done

---
+++ day09/ex02bis/todo.js
@@ -0,0 +1,58 @@
+$('#new_task').click(ft_onClick)
+
+const cookies = document.cookie.split(';').reduce((cookies, cookie) => {
+    const [ name, value ] = cookie.split('=').map(c => c.trim());
+    cookies[name] = value;
+    return cookies;
+}, {});
+
+let i = 0
+const keys = Object.keys(cookies)
+
+if (keys[0] !== "") {
+    i = keys.length
+    let values = Object.values(cookies)
+    values.forEach((val, index) => {
+        const div = document.createElement('div')
+        div.className = `item-${keys[index]}`
+        div.id = keys[index]
+        div.innerHTML = `<a href="#" onclick=" ft_deleteTodo(${keys[index]})">${val}</a>`
+        $("#ft_list").prepend(div)
+    })
+}
+   
+function  ft_deleteTodo(id) {
+    let el = document.getElementById(id)
+    const confirm = window.confirm(`Are you sure you want to delete todo ${el.textContent}?`);
+    if (confirm) {
+        $("#ft_list").remove(el)
+        ft_deleteCookie(id)
+    } 
+}
+
+function  ft_addTodo(todo) {
+    todo = todo.trim()
+    const div = document.createElement('div')
+    div.className = `item-${i}`
+    div.id = i
+    div.innerHTML = `<a href="#" onclick=" ft_deleteTodo(${i})">${todo}</a>`
+    $("#ft_list").prepend(div)
+     ft_createCookie(i++, todo, 7)
+}
+
+function  ft_onClick () {
+    let input = window.prompt("Add a new task:")
+    if (input)
+         ft_addTodo(input)
+}
+
+function  ft_createCookie(cookieName,cookieValue,daysToExpire) {
+    let date = new Date()
+    date.setTime(date.getTime() + (daysToExpire * 24 * 60 * 60 * 1000))
+    document.cookie = cookieName + "=" + cookieValue + "; expires=" + date.toGMTString()
+}
+
+function  ft_deleteCookie( name ) {
+    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    window.location.reload()
+  }
